6c98b64965f3562806bfeb7789a842b94d06d571
Charles Stahl <cnstahl@princeton.edu>
2017-05-01 00:14:38 -0400

    entropy calcs

--- python/entropy.sh
+++ python/entropy.sh
@@ -10,11 +10,7 @@
 #SBATCH --output=res.txt
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1 
-#SBATCH  --mem=5000
-#SBATCH --time=020:00:00
+#SBATCH  --mem=4000
+#SBATCH --time=01:00:00
 
-for N in 12
-do 
-    printf "\nComputing N = " $N
-    python count_ground_states.py $N > ../data/ground\ state\ counts$N.txt
-done
+python entropy.py
